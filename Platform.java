3fdd1edac9219dcc099433667234e4d6e64f58f9
robertvazquez1 <152912749+robertvazquez1@users.noreply.github.com>
2023-12-06 19:36:57 +0000

    platform

--- Platform.java
+++ Platform.java
@@ -1,5 +1,18 @@
 //class for generic platform
 
-public class Platform implements Spawneable{
+public class Platform implements Spawnable
+{
+    private int xPos;
+    private int yPos;
+    private int width;
+    private int height;
 
+    public Platform(x, y, w, h){
+        xPos=x;
+        yPos=y;
+        width=w;
+        height=h;
+    }
+
+    
 }
