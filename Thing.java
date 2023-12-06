4a78354da0515afe0e48727b421fc4b3a3efed85
robertvazquez1 <152912749+robertvazquez1@users.noreply.github.com>
2023-12-06 20:24:42 +0000

    thing class

---
+++ Thing.java
@@ -0,0 +1,50 @@
+public class Thing{
+    private int xPos;
+    private int yPos;
+    private int width;
+    private int height;
+
+    public Thing(int x, int y, int w, int h){
+        xPos = x;
+        yPos = y;
+        width = w;
+        height = h;
+    }
+
+    public void setPos(int x, int y){
+        xPos = x;
+        yPos = y;
+    }
+
+    public void setX(int x){
+        xPos = x;
+    }
+
+    public void setY(int y){
+        yPos = y;
+    }
+
+    public void setWidth(int w){
+        width = w;
+    }
+
+    public void setHeight(int h){
+        height = h;
+    }
+
+    public int getX(){
+        return xPos;
+    }
+
+    public int getY(){
+        return yPos;
+    }
+
+    public int getWidth(){
+        return width;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+}
