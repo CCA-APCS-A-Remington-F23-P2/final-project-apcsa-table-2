198cc7c284af29096852af308b6e99db19832abc
DziukT <152912704+DziukT@users.noreply.github.com>
2023-12-06 19:01:27 +0000

    dog

--- Dog.java
+++ Dog.java
@@ -1,10 +1,17 @@
 public class Dog{
     private int xPos;
     private int yPos;
-    
+    private int width;
+    private int height;
+    private int speed;
     private int jumpHeight;
 
-    public Dog(){
-        this(x,y,jumpHeight)
+    public Dog(int x, int y, int w, int h, int spd, int jH){
+        xPos = x;
+        yPos = y;
+        width = w;
+        height = h;
+        speed = spd;
+        jumpHeight = jH;
     }
 }
