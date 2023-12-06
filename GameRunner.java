63e368dfbec8aab323a5abbb9b289a49b6d2a455
koe-g <144854236+koe-g@users.noreply.github.com>
2023-12-06 19:21:57 +0000

    GameRunner graphics done

--- GameRunner.java
+++ GameRunner.java
@@ -12,7 +12,7 @@ import java.awt.image.BufferedImage;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class OuterSpace extends Canvas implements KeyListener, Runnable
+public class GameRunner extends Canvas implements KeyListener, Runnable
 {
   private Dog dog;
 
@@ -26,8 +26,7 @@ public class OuterSpace extends Canvas implements KeyListener, Runnable
     keys = new boolean[2];
 
     //instantiate other instance variables
-    //Ship, Alien
-    dog = new Dog(250,250,50,50,5);
+    dog = new Dog();
 
     this.addKeyListener(this);
     new Thread(this).start();
@@ -55,46 +54,24 @@ public class OuterSpace extends Canvas implements KeyListener, Runnable
     Graphics graphToBack = back.createGraphics();
   
     graphToBack.setColor(Color.CYAN);
-    graphToBack.drawString("StarFighter ", 25, 50 );
-    graphToBack.setColor(Color.BLACK);
-    graphToBack.fillRect(0,0,800,600);
+    graphToBack.fillRect(0,0,200,400);
   
     if (keys[0])
     {
-      ship.move("LEFT");
+      dog.move("LEFT");
     }
     if (keys[1])
     {
-      ship.move("RIGHT");
-    }
-    if (keys[2])
-    {
-      ship.move("UP");
+      dog.move("RIGHT");
     }
       
     //add code to move Ship, Alien, etc.
     dog.draw(graphToBack);
-    alienHorde.draw(graphToBack);
-    alienHorde.move();
-    ammo.draw(graphToBack);
-    ammo.move();
-    alienAmmo.draw(graphToBack);
-    alienAmmo.move();
-    graphToBack.setColor(Color.CYAN);
-    graphToBack.drawString("Score: " + score, 10, 20);
-    graphToBack.drawString("Lives: " + lives, 10, 35);
     
       
     //add in collision detection to see if Bullets hit the Aliens and if Bullets hit the Ship
   
   
-    for(int i=0; i<alienHorde.getList().size(); i++){
-      if(alienHorde.getList().get(i).didCollide(ship)){
-        alienHorde.getList().remove(i);
-        lives--;
-        livesLost = true;
-      }
-    }
   
     twoDGraph.drawImage(back, null, 0, 0);
   }
@@ -102,59 +79,26 @@ public class OuterSpace extends Canvas implements KeyListener, Runnable
 
   public void keyPressed(KeyEvent e)
   {
-    if (e.getKeyCode() == KeyEvent.VK_LEFT)
+    if (e.getKeyCode() == KeyEvent.VK_A)
     {
       keys[0] = true;
     }
-    if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+    if (e.getKeyCode() == KeyEvent.VK_D)
     {
       keys[1] = true;
-    }
-    if (e.getKeyCode() == KeyEvent.VK_UP)
-    {
-      keys[2] = true;
-    }
-    if (e.getKeyCode() == KeyEvent.VK_DOWN)
-    {
-      keys[3] = true;
-    }
-    if (e.getKeyCode() == KeyEvent.VK_SPACE)
-    {
-      keys[4] = true;
-    }
-    if (e.getKeyCode() == KeyEvent.VK_R)
-    {
-      keys[5] = true;
     }
     repaint();
   }
 
   public void keyReleased(KeyEvent e)
   {
-    if (e.getKeyCode() == KeyEvent.VK_LEFT)
+    if (e.getKeyCode() == KeyEvent.VK_A)
     {
       keys[0] = false;
     }
-    if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+    if (e.getKeyCode() == KeyEvent.VK_D)
     {
       keys[1] = false;
-    }
-    if (e.getKeyCode() == KeyEvent.VK_UP)
-    {
-      keys[2] = false;
-    }
-    if (e.getKeyCode() == KeyEvent.VK_DOWN)
-    {
-      keys[3] = false;
-    }
-    if (e.getKeyCode() == KeyEvent.VK_SPACE)
-    {
-      keys[4] = false;
-      spacePressed = false;
-    }
-    if (e.getKeyCode() == KeyEvent.VK_R)
-    {
-      keys[5] = false;
     }
     repaint();
   }
