1843e9d8f9a05fd89080337f63f1f338667992c9
DziukT <152912704+DziukT@users.noreply.github.com>
2023-12-06 19:08:45 +0000

    added dog stuff

--- Dog.java
+++ Dog.java
@@ -1,4 +1,4 @@
-public class Dog{
+public class Dog implements Collideable{
     private int xPos;
     private int yPos;
     private int width;
@@ -14,4 +14,59 @@ public class Dog{
         speed = spd;
         jumpHeight = jH;
     }
+
+    public void setPos(int x, int y){
+        xPos = x;
+        yPos = y;
+    }
+
+    public void setX(int x){
+        xPos = x;
+    }
+
+    public void setY(int y){
+        yPos = y;
+    }
+
+    public void setWidth(int w){
+        width = w;
+    }
+
+    public void setHeight(int h){
+        height = h;
+    }
+
+    public void setSpeed(int s){
+        speed = s;
+    }
+
+    public void setJumpHeight(int j){
+        jumpHeight = j;
+    }
+
+    public int getX(){
+        return xPos;
+    }
+
+    public int getY(){
+        return yPos;
+    }
+
+    public int getWidth(){
+        return width;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+
+    public int getSpeed(){
+        return speed;
+    }
+
+    public int getJumpHeight(){
+        return jumpHeight;
+    }
+
+
 }
