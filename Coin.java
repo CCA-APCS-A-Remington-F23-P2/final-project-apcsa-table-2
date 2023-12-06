0312ce476205d63bccb94fdf2de257db1930bed5
DziukT <152912704+DziukT@users.noreply.github.com>
2023-12-06 19:36:08 +0000

    bug fixes

--- Coin.java
+++ Coin.java
@@ -1,9 +1,52 @@
 public class Coin implements Collideable, Spawnable{
 
+    private int xPos;
+    private int yPos;
+    private int width;
+    private int height;
 
+    public Coin(int x, int y, int w, int h){
+        xPos = x;
+        yPos = y;
+        width = w;
+        height = h;
+    }
 
-    public Coin(){
+    public void setPos(int x, int y){
+        xPos = x;
+        yPos = y;
+    }
 
+    public void setX(int x){
+        xPos = x;
+    }
+
+    public void setY(int y){
+        yPos = y;
+    }
+
+    public void setWidth(int w){
+        width = w;
+    }
+
+    public void setHeight(int h){
+        height = h;
+    }
+
+    public int getX(){
+        return xPos;
+    }
+
+    public int getY(){
+        return yPos;
+    }
+
+    public int getWidth(){
+        return width;
+    }
+
+    public int getHeight(){
+        return height;
     }
 
     public boolean didCollide(Object other){
--- Obstacle.java
+++ Obstacle.java
@@ -1,7 +1,52 @@
 public class Obstacle implements Collideable, Spawnable{
 
-    public Obstacle(){
+    private int xPos;
+    private int yPos;
+    private int width;
+    private int height;
 
+    public Obstacle(int x, int y, int w, int h){
+        xPos = x;
+        yPos = y;
+        width = w;
+        height = h;
+    }
+
+    public void setPos(int x, int y){
+        xPos = x;
+        yPos = y;
+    }
+
+    public void setX(int x){
+        xPos = x;
+    }
+
+    public void setY(int y){
+        yPos = y;
+    }
+
+    public void setWidth(int w){
+        width = w;
+    }
+
+    public void setHeight(int h){
+        height = h;
+    }
+
+    public int getX(){
+        return xPos;
+    }
+
+    public int getY(){
+        return yPos;
+    }
+
+    public int getWidth(){
+        return width;
+    }
+
+    public int getHeight(){
+        return height;
     }
 
     public boolean didCollide(Object other){
