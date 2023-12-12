6889253530afa02d753f2ee41466d5402900c3a4
DziukT <152912704+DziukT@users.noreply.github.com>
2023-12-12 19:41:54 +0000

    changed coin to picture

--- Coin.java
+++ Coin.java
@@ -1,10 +1,24 @@
 import java.awt.Graphics;
 import java.awt.Color;
+import java.io.File;
+import java.net.URL;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Coin extends Thing implements Collideable{
 
+  private Image image;
+
   public Coin(int x, int y, int w, int h){
       super(x,y,w,h,1);
+    try{
+      URL url = getClass().getResource("CoinPic.png");
+      image = ImageIO.read(url);
+    }
+    catch (Exception e){
+    }
   }
   public boolean didCollide(Thing o){
     if(this.getX() + this.getWidth()>= o.getX() && this.getX() <= o.getX()+o.getWidth() && this.getY() + this.getHeight() >=o.getY() && this.getY()<=o.getY()+o.getHeight()){
@@ -14,8 +28,7 @@ public class Coin extends Thing implements Collideable{
     }
 
   public void draw(Graphics window){
-    window.setColor(Color.YELLOW);
-    window.fillRect(getX(),getY(),getWidth(),getHeight());
+    window.drawImage(image,getX(),getY(),getWidth(),getHeight(),null);
   }
 
   public String toString(){
